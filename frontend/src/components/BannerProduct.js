3eea3f44f0ed8c164e3baff8ac7ae8d71194865d
srivasthava1212 <srivats1212@gmail.com>
2025-03-20 19:00:19 +0000

    banner added

--- frontend/src/components/BannerProduct.js
+++ frontend/src/components/BannerProduct.js
@@ -5,11 +5,11 @@ import image3 from "../assets/banner/img3.avif";
 import image4 from "../assets/banner/img4.jpg";
 import image5 from "../assets/banner/img5.jpg";
 
-import image1Mobile from "../assets/banner/img1_mobile.jpg";
-import image2Mobile from "../assets/banner/img2_mobile.webp";
-import image3Mobile from "../assets/banner/img3_mobile.jpg";
-import image4Mobile from "../assets/banner/img4_mobile.jpg";
-import image5Mobile from "../assets/banner/img5_mobile.png";
+import image1Mobile from "../assets/banner/img1.jpg";
+import image2Mobile from "../assets/banner/img2.webp";
+import image3Mobile from "../assets/banner/img3.avif";
+import image4Mobile from "../assets/banner/img4.jpg";
+import image5Mobile from "../assets/banner/img5.jpg";
 
 import { FaAngleRight } from "react-icons/fa6";
 import { FaAngleLeft } from "react-icons/fa6";
